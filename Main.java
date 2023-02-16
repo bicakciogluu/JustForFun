9aee2f8f0bfb00ee146b5f985379bf63ea7ed6d9
Muhammed Furkan Başıbüyük <furkan.basibuyuk@ug.bilkent.edu.tr>
2023-02-16 21:20:26 +0300

    Main2

--- Main.java
+++ Main.java
@@ -6,20 +6,44 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Enter number to be examined: ");
+        int number = in.nextInt();
 
         //Fibonacci
+        boolean isFibonacci;
+        System.out.println(number + " is ");
+        
+        if(!isFibonacci) System.out.println("not");
 
+        System.out.println(" a Fibonacci number.");
 
         //Harshad
+
+        boolean isHarshad;
+        System.out.println(number + " is ");
+        
+        if(!isHarshad) System.out.println("not");
+
+        System.out.println(" a Harshad number.");
 
 
         //Prime Number
 
+        boolean isPrime;
+        System.out.println(number + " is ");
+        
+        if(!isPrime) System.out.println("not");
 
+        System.out.println(" a Prime number.");
 
         //Narcissistic number
 
+        boolean isNarcissistic;
+        System.out.println(number + " is ");
+        
+        if(!isNarcissistic) System.out.println("not");
+
+        System.out.println(" a Narcissistic number.");
 
     }
 }
