071966f8b6bd6f2821dd3f486339ed828ec52bc0
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2023-02-16 21:44:46 +0300

    Auto stash before merge of "main" and "origin/main"

--- Main.java
+++ Main.java
@@ -1,49 +1,73 @@
 
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number to be examined: ");
         int number = in.nextInt();
 
         System.out.println("Number of digits: " + Harshad.numberOfDigits(number));
 
-        //Fibonacci
+        // Fibonacci
         boolean isFibonacci = fibonacci.isFibonacci(number);
         System.out.print(number + " is ");
+<<<<<<< Updated upstream
         
         if(!isFibonacci) System.out.print("not ");
+=======
+
+        if (!isFibonacci)
+            System.out.print("not");
+>>>>>>> Stashed changes
 
         System.out.println("a Fibonacci number.");
 
-        //Harshad
+        // Harshad
 
         boolean isHarshad = Harshad.harsahdChecker(number);
         System.out.print(number + " is ");
+<<<<<<< Updated upstream
         
         if(!isHarshad) System.out.print("not ");
 
         System.out.println("a Harshad number.");
+=======
 
+        if (!isHarshad)
+            System.out.print("not");
+>>>>>>> Stashed changes
 
-        //Prime Number
+        System.out.println(" a Harshad number.");
+
+        // Prime Number
 
         boolean isPrime = Prime.isPrime(number);
         System.out.print(number + " is ");
+<<<<<<< Updated upstream
         
         if(!isPrime) System.out.print("not ");
+=======
+
+        if (!isPrime)
+            System.out.print("not");
+>>>>>>> Stashed changes
 
         System.out.println("a Prime number.");
 
-        //Narcissistic number
+        // Narcissistic number
 
         boolean isNarcissistic = narcissistic.isNarcissistic(number);
         System.out.print(number + " is ");
+<<<<<<< Updated upstream
         
         if(!isNarcissistic) System.out.print("not ");
+=======
+
+        if (!isNarcissistic)
+            System.out.print("not");
+>>>>>>> Stashed changes
 
         System.out.println("a Narcissistic number.");
 
